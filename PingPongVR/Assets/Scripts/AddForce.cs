7cc1d90000ea39588134e093ac77736cd8e64df1
ultoman <jbonasera@anl.gov>
2018-08-01 11:45:12 -0500

    Added physics

---
+++ PingPongVR/Assets/Scripts/AddForce.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddForce : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float xForce, yForce, zForce;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		rb.AddForce(new Vector3(xForce,yForce,zForce));
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		//rb.AddForce(new Vector3(0,-70,0));
+		
+	}
+}
