6fd39d416e5aaec174e15d28c9b2e3fd8b049a60
ultoman <jbonasera@anl.gov>
2018-08-01 16:37:26 -0500

    Added Script to Paddle

---
+++ PingPongVR/Assets/Scripts/Paddle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log(GetComponent<Rigidbody>().velocity.magnitude);
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("Triggered");
+		if (other.gameObject.tag == "Ball")
+		{
+			Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
+			Debug.Log(rb.velocity.sqrMagnitude);
+			rb.velocity = transform.forward * (rb.velocity.magnitude + 0.5f);
+			
+			
+		}
+	}
+}
