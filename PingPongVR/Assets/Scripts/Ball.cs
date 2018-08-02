fd89e930a6349e9f1ccccff217b82bc3b15954ba
Rob Kondratowicz <robkondratowicz@gmail.com>
2018-08-02 16:19:34 -0500

    Added Walls and balls spawning

--- PingPongVR/Assets/Scripts/Ball.cs
+++ PingPongVR/Assets/Scripts/Ball.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour {
 
+    public float _maxSpeed = 10f;
+
+    public Vector2 _curve;
+
 	private GameObject _lastObjectHit;
+    private Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
-		
+        rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        if(rb.velocity.magnitude > _maxSpeed)
+        {
+            rb.velocity = rb.velocity.normalized * _maxSpeed;
+        }
 	}
 
 	void OnTriggerEnter(Collider other)
---
+++ PingPongVR/Assets/Scripts/BallSpawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawn : MonoBehaviour {
+    
+    private SteamVR_TrackedController controller;
+    public GameObject ballPrefab;
+    public Transform ballSpawnLocation;
+
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<SteamVR_TrackedController>();
+        controller.TriggerClicked += spawnBall;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void spawnBall(object sender, ClickedEventArgs e)
+    {
+        GameObject.Instantiate(ballPrefab, ballSpawnLocation);
+    }
+}
